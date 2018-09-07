f31485c4e8f22fdfa476cb58c9cd7f35d8643d6e
smallz <wangyang@samsundot.com>
2018-09-07 17:56:42 +0800

    ASP.NET Core依赖注入

---
+++ web/ASPDotNETCoreStudy/DI_Sample/FlagRepositoryForUser.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+namespace DI_Sample
+{
+    internal class FlagRepositoryForUser : IFlagRepository
+    {
+        private ClaimsPrincipal user;
+
+        public FlagRepositoryForUser(ClaimsPrincipal user)
+        {
+            this.user = user;
+        }
+
+        public Flag GetFlag(string country)
+        {
+            return new Flag();
+        }
+    }
+}
