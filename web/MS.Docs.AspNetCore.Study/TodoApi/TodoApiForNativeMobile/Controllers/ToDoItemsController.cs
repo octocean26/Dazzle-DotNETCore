25eccb61e3f62189e18118f2cf9d8e4159d49887
octocean26 <octoberocean@163.com>
2018-11-08 14:20:16 +0800

    针对移动端调用的WebAPI教程示例代码

---
+++ web/MS.Docs.AspNetCore.Study/TodoApi/TodoApiForNativeMobile/Controllers/ToDoItemsController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.UI.Pages;
+using Microsoft.AspNetCore.Mvc;
+using TodoApiForNativeMobile.Models;
+
+namespace TodoApiForNativeMobile.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToDoItemsController : Controller
+    {
+        private readonly IToDoRepository _toDoRepository;
+
+
+        public ToDoItemsController(IToDoRepository toDoRepository)
+        {
+            this._toDoRepository = toDoRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return Ok(_toDoRepository.All);
+        }
+
+        //由于 item 参数将在 POST 的正文中传递，因此该参数用 [FromBody] 属性修饰
+        [HttpPost]
+        public IActionResult Create([FromBody] ToDoItem item)
+        {
+            try
+            {
+                if (item == null || !ModelState.IsValid)
+                {
+                    return BadRequest(ErrorCode.TodoItemNameAndNotesRequired.ToString());
+                }
+
+                bool itemExists = _toDoRepository.DoesItemExist(item.ID);
+                if (itemExists)
+                {
+                    return StatusCode(StatusCodes.Status409Conflict, ErrorCode.TodoItemIDInUse.ToString());
+                }
+                _toDoRepository.Insert(item);
+            }
+            catch (Exception)
+            {
+                return BadRequest(ErrorCode.CouldNotCreateItem.ToString());
+            }
+            return Ok(item);
+        }
+         
+        
+        [HttpPut]
+        public IActionResult Edit([FromBody] ToDoItem item)
+        {
+            try
+            {
+                if(item==null||!ModelState.IsValid)
+                {
+                    return BadRequest(ErrorCode.TodoItemNameAndNotesRequired.ToString());
+                }
+                var existingItem = _toDoRepository.Find(item.ID);
+                if(existingItem==null)
+                {
+                    return NotFound(ErrorCode.RecordNotFound.ToString());
+                }
+                _toDoRepository.Update(item);
+            }
+            catch (Exception)
+            {
+                return BadRequest(ErrorCode.CouldNotUpdateItem.ToString());
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            try
+            {
+                var item = _toDoRepository.Find(id);
+                if(item==null)
+                {
+                    return NotFound(ErrorCode.RecordNotFound.ToString());
+                }
+                _toDoRepository.Delete(id);
+            }
+            catch (Exception)
+            {
+                return BadRequest(ErrorCode.CouldNotDeleteItem.ToString());
+            }
+            return NoContent();
+        }
+    }
+}
---
+++ web/MS.Docs.AspNetCore.Study/TodoApi/TodoApiForNativeMobile/Models/ToDoItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApiForNativeMobile.Models
+{
+    public class ToDoItem
+    {
+        [Required]
+        public string ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Notes { get; set; }
+
+        [Required]
+        public bool Done { get; set; }
+
+
+
+    }
+}
