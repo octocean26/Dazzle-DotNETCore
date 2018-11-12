26aa046d65f3eb0189559975c943cd5fa7b91855
octocean26 <octoberocean@163.com>
2018-11-12 16:47:15 +0800

    DI关系概述

---
+++ web/MS.Docs.AspNetCore.Study/Docs.DI/Docs.DI/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Docs.DI.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace Docs.DI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IMyDependency _myDependency;
+        public IndexModel(IMyDependency myDependency)
+        {
+            _myDependency = myDependency;
+        }
+        public async Task OnGetAsync()
+        {
+            await _myDependency.WriteMessage("test");
+        }
+    }
+}
