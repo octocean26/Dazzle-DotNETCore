72f428dedde6c847c6bc6626c1dc8de3df8a7312
octocean26 <octoberocean@163.com>
2018-11-26 17:17:35 +0800

    配置中的ConfigureAppConfiguration的使用

---
+++ web/MS.Docs.AspNetCore.Study/Docs.Configuration/Docs.Configuration/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Docs.Configuration
+{
+    public class Program
+    {
+        //键值对的快速赋值语法
+        public static Dictionary<string, string> arrayDict = new Dictionary<string, string> {
+            { "array:entries:0","value0"},
+            { "array:entries:1","value1"},
+            { "array:entries:2","value2"},
+            { "array:entries:3","value3"},
+            { "array:entries:4","value4"},
+            { "array:entries:5","value5"}
+
+        };
+
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            //指定应用的配置提供程序
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.SetBasePath(Directory.GetCurrentDirectory());
+                config.AddInMemoryCollection(arrayDict);
+                config.AddJsonFile("json_array.json", optional: false, reloadOnChange: false);
+                config.AddJsonFile("startship.json", optional: false, reloadOnChange: false);
+                config.AddXmlFile("tvshow.xml", optional: false, reloadOnChange: false);
+                config.AddEFConfiguration(options => options.UseInMemoryDatabase("InMemoryDb"));
+                config.AddCommandLine(args);
+            })
+                .UseStartup<Startup>();
+    }
+}
