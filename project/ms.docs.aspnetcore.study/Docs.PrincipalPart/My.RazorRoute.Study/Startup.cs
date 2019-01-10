940c096816ba043ef67ade6978688acb54f36087
octocean26 <octoberocean@163.com>
2019-01-10 15:22:26 +0800

    ASP.NET Core Razor页面路由学习项目

---
+++ project/ms.docs.aspnetcore.study/Docs.PrincipalPart/My.RazorRoute.Study/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace My.RazorRoute.Study
+{
+
+    public class Startup
+    {
+        private readonly ILogger logger;
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(ILogger<Startup> _logger, IConfiguration configuration)
+        {
+            this.logger = _logger;
+            this.Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMvc();
+
+            
+        }
+    }
+}
