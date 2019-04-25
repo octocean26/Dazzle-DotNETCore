0d2e1ca1a07278f323d418cc240b034d9725167a
octocean26 <octoberocean@163.com>
2019-04-25 16:40:17 +0800

    ASP.NET Core MVC中的应用程序模型（高级）暂停更新

---
+++ project/ms.docs.aspnetcore.study/Docs.PrincipalPart/My.MvcApplicationModel.Study/Conventions/CustomActionNameAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace My.MvcApplicationModel.Study.Conventions
+{
+    public class CustomActionNameAttribute : Attribute, IActionModelConvention
+    {
+        private readonly string _actionName;
+
+        public CustomActionNameAttribute(string actionName){
+            _actionName = actionName;
+        }
+
+        public void Apply(ActionModel action)
+        {
+            action.ActionName = _actionName;
+
+        }
+    }
+}
---
+++ project/ms.docs.aspnetcore.study/Docs.PrincipalPart/My.MvcApplicationModel.Study/Conventions/NamespaceRoutingConvention.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace My.MvcApplicationModel.Study.Conventions
+{
+    public class NamespaceRoutingConvention : IApplicationModelConvention
+    {
+   
+        public void Apply(ApplicationModel application)
+        {
+            foreach(var controller in application.Controllers){
+                var hasAttributeRouteModels = controller.Selectors.Any(a => a.AttributeRouteModel != null);
+                if(!hasAttributeRouteModels
+                && controller.ControllerName.Contains("Namespace"))
+                {
+                    controller.Selectors[0].AttributeRouteModel = new AttributeRouteModel()
+                    {
+                        Template = controller.ControllerType.Namespace.Replace('.', '/') + "/[controller]/[action]/{id?}"
+                    };
+                }
+            }
+        }
+    }
+}
