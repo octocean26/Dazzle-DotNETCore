e642a9b327eb1d270faaf8848febd3cf0ba564d4
octocean26 <octoberocean@163.com>
2019-04-26 20:25:39 +0800

    ASP.NET Core MVC Filters（未完结）

--- project/ms.docs.aspnetcore.study/Docs.PrincipalPart/My.MvcFilters.Study/Controllers/HomeController.cs
+++ project/ms.docs.aspnetcore.study/Docs.PrincipalPart/My.MvcFilters.Study/Controllers/HomeController.cs
@@ -1,17 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using My.MvcFilters.Study.Filters;
 
 namespace My.MvcFilters.Study.Controllers
 {
-    public class HomeController:Controller
+    [AddHeader("Author", "Smallz")]
+    public class HomeController : Controller
     {
-        public IActionResult Index(){
+        public IActionResult Index()
+        {
             return View();
         }
-        
+
+        public IActionResult Test()
+        {
+            return Content("测试");
+        }
+
+        [ShortCircuitingResourceFilter]
+        public IActionResult SomeResource()
+        {
+            return Content("成功访问到标头资源");
+        }
+
+        [AddHeaderWithFactory]
+        public IActionResult HeaderWithFactory()
+        {
+            return Content("AddHeaderWithFactory");
+        }
+
+        [ServiceFilter(typeof(AddHeaderFilterWithDi))]
+        public IActionResult ServiceFilterTest()
+        {
+            return Content("ServiceFilter应用");
+        }
+
+        [TypeFilter(typeof(LogConstantFilter),
+                Arguments = new object[] { "Method 'Hi' called" })]
+        public IActionResult Hi(string name)
+        {
+            return Content("Hi," + name);
+        }
+
+        [SampleActionFilter]
+        public IActionResult HiFactory(){
+            return Content("在属性上实现IFilterFactory");
+        }
     }
 }
--- project/ms.docs.aspnetcore.study/Docs.PrincipalPart/My.MvcFilters.Study/Startup.cs
+++ project/ms.docs.aspnetcore.study/Docs.PrincipalPart/My.MvcFilters.Study/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using My.MvcFilters.Study.Filters;
 
 namespace My.MvcFilters.Study
 {
@@ -15,8 +16,15 @@ namespace My.MvcFilters.Study
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
+            services.AddMvc(options=> {
+                options.Filters.Add(new AddHeaderAttribute("GlobalAddHeader", "Global Smalzz"));
+                options.Filters.Add(typeof(SampleActionFilter));
+                options.Filters.Add(typeof(SampleAsyncActionFilter));
+                //options.Filters.Add(new SampleGlobalActionFilter())
+            })
             .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
+
+            services.AddScoped<AddHeaderFilterWithDi>(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
