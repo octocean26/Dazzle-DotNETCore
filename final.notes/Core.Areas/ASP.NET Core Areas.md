bd4b7425af0a2f79744389067b0ea32e608aea45
octocean26 <octoberocean@163.com>
2019-05-05 19:44:27 +0800

    ASP.NET Core Areas notes

--- final.notes/Core.Areas/ASP.NET Core Areas.md
+++ final.notes/Core.Areas/ASP.NET Core Areas.md
@@ -1,3 +1,12 @@
 # ASP.NET Core中的区域
 
+
+
+
+
+
+
+
+
 https://docs.microsoft.com/zh-cn/aspnet/core/mvc/controllers/routing?view=aspnetcore-2.2#areas
+
---
+++ project/ms.docs.aspnetcore.study/Docs.PrincipalPart/My.Area.Study/Areas/Wy/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace My.Area.Study.Areas.Wy.Controllers
+{
+    [Area("Wy")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Test(){
+            return "Wy Area";
+        }
+    }
+}
