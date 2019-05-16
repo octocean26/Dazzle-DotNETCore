f6ca9c74f893fdfcc3e8e2af68031462a5a30875
octocean26 <octoberocean@163.com>
2019-05-16 17:52:35 +0800

    自定义模型绑定

---
+++ project/ms.docs.aspnetcore.study/Docs.PrincipalPart/My.CustomModelBinding.Study/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace My.CustomModelBinding.Study.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students {get;set;}
+    }
+}
---
+++ project/ms.docs.aspnetcore.study/Docs.PrincipalPart/My.CustomModelBinding.Study/Data/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using My.CustomModelBinding.Study.Binders;
+
+namespace My.CustomModelBinding.Study.Data
+{
+[ModelBinder(BinderType =typeof(StudentEntityBinder))]
+    public class Student
+    {
+        public int Id{ get; set; }
+        public string Name{ get; set; }
+        public string StuCode{ get; set; }
+      
+       
+        
+    }
+}
